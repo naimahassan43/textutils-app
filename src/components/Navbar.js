6a5ccbbb85e28343124b815dd5f31cec9e630aa0
Naima Hassan <naima.hassan434@gmail.com>
2021-11-09 22:52:36 +0000

    Enable dark mode using switch checkbox

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,9 @@ import PropTypes from "prop-types";
 const Navbar = (props) => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav
+        className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
+      >
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
             {props.title}
@@ -33,7 +35,7 @@ const Navbar = (props) => {
                 </a>
               </li>
             </ul>
-            <form className="d-flex">
+            {/* <form className="d-flex">
               <input
                 className="form-control me-2"
                 type="search"
@@ -43,7 +45,25 @@ const Navbar = (props) => {
               <button className="btn btn-outline-success" type="submit">
                 Search
               </button>
-            </form>
+            </form> */}
+            <div
+              className={`form-check form-switch text-${
+                props.mode === "light" ? "dark" : "light"
+              }`}
+            >
+              <input
+                className="form-check-input"
+                type="checkbox"
+                onClick={props.toggleMode}
+                id="flexSwitchCheckChecked"
+              />
+              <label
+                className="form-check-label"
+                htmlFor="flexSwitchCheckChecked"
+              >
+                Enable Dark Mode
+              </label>
+            </div>
           </div>
         </div>
       </nav>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -11,8 +11,22 @@ function TextForm(props) {
     let newText = text.toLowerCase();
     setText(newText);
   };
+  const handleClearClick = () => {
+    // console.log("uppercase was clicked: "+ text);
+    let newText = "";
+    setText(newText);
+  };
   const handleOnChange = (event) => {
     setText(event.target.value);
+  };
+  const handleCopy = () => {
+    let text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
   };
   const [text, setText] = useState("");
   return (
@@ -28,11 +42,23 @@ function TextForm(props) {
             rows={8}
           />
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>
+        <button className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>
           Convert to Uppercase
         </button>
-        <button className="btn btn-primary mx-2" onClick={handleLowClick}>
+        <button className="btn btn-info mx-2 my-2" onClick={handleLowClick}>
           Convert to Lowercase
+        </button>
+        <button className="btn btn-success mx-2 my-2" onClick={handleCopy}>
+          Copy Text
+        </button>
+        <button
+          className="btn btn-secondary mx-2 my-2"
+          onClick={handleExtraSpaces}
+        >
+          Remove Extra Spaces
+        </button>
+        <button className="btn btn-danger mx-2 my-2" onClick={handleClearClick}>
+          Clear Text
         </button>
       </div>
       <div className="container my-3">
