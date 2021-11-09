9a478d422299bdd644eef29f8601484702ef418b
Naima Hassan <naima.hassan434@gmail.com>
2021-11-09 19:24:54 +0000

    text form components using useState hook

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,13 +6,18 @@ function TextForm(props) {
     let newText = text.toUpperCase();
     setText(newText);
   };
+  const handleLowClick = () => {
+    // console.log("uppercase was clicked: "+ text);
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
   const handleOnChange = (event) => {
     setText(event.target.value);
   };
-  const [text, setText] = useState("Enter text here");
+  const [text, setText] = useState("");
   return (
-    <div>
-      <div>
+    <>
+      <div className="container">
         <h1>{props.heading}</h1>
         <div className="mb-3">
           <textarea
@@ -26,8 +31,20 @@ function TextForm(props) {
         <button className="btn btn-primary" onClick={handleUpClick}>
           Convert to Uppercase
         </button>
+        <button className="btn btn-primary mx-2" onClick={handleLowClick}>
+          Convert to Lowercase
+        </button>
       </div>
-    </div>
+      <div className="container my-3">
+        <h3>Your Text Summary</h3>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>Need {0.008 * text.split(" ").length} Minutes to read</p>
+        <h4>Preview</h4>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
 
