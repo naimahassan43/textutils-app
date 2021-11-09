4378ffcefb9ebeef0f4df14b93bc33d822759818
Naima Hassan <naima.hassan434@gmail.com>
2021-11-09 23:59:32 +0000

    Alert message for dark mode

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import { useState } from "react";
 import "./App.css";
+import Alert from "./components/Alert";
 // import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
 function App() {
   const [mode, setMode] = useState("light"); // whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+  };
   const toggleMode = () => {
     if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "#2d5e68";
+      showAlert("Dark Mode has been enabled", "success");
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
+      showAlert("Light Mode has been enabled", "success");
     }
   };
   return (
@@ -23,6 +34,7 @@ function App() {
         mode={mode}
         toggleMode={toggleMode}
       />
+      <Alert alert={alert} />
       <div className="container my-3">
         <TextForm heading="Enter the text to analyze below" mode={mode} />
         {/* <About /> */}
