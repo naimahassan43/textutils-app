b1b06e6984b41b9134fec33a3a75470176a466ea
Naima Hassan <naima.hassan434@gmail.com>
2021-11-09 15:08:02 +0000

    Text form handle on useState hook

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <>
       <Navbar title="TextUtil" aboutText="About Us" />
       <div className="container my-3">
-        <TextForm heading="Enter The Text to Analyze" />
+        <TextForm heading="Enter the text to analyze below" />
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 function TextForm(props) {
+  const handleUpClick = () => {
+    // console.log("uppercase was clicked: "+ text);
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+  const [text, setText] = useState("Enter text here");
   return (
     <div>
       <div>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-          <label htmlFor="myBox" className="form-label">
-            Example textarea
-          </label>
-          <textarea className="form-control" id="myBox" rows={8} />
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows={8}
+          />
         </div>
+        <button className="btn btn-primary" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
       </div>
     </div>
   );
